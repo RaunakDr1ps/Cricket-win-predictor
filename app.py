cd9285fa983311c3b69e6461d9c18e50c8e3c38d
RaunakDr1ps <rubiuszhindi@gmail.com>
2026-04-24 09:13:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ teams = [
 team_badges = {
     "Chennai Super Kings": "CSK",
     "Mumbai Indians": "MI",
-    "Royal Challengers Bangalore": "(︶︿︶)_╭∩╮",
+    "Royal Challengers Bangalore": "╭∩╮",
     "Kolkata Knight Riders": "KKR",
     "Delhi Capitals": "DC",
     "Punjab Kings": "PBKS",
